5057f999d31564230e4782a9d67fb62b2e581262
Aaron Ogle <atogle@gmail.com>
2012-03-23 16:11:11 -0400

    add a stub view for routes

--- src/project/urls.py
+++ src/project/urls.py
@@ -19,4 +19,5 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html')),
 
     url(r'^stops/?$', 'project.views.stops', name='stops'),
+    url(r'^routes/?$', 'project.views.routes', name='routes'),
 )
--- src/project/views.py
+++ src/project/views.py
@@ -1,19 +1,40 @@
 from django.contrib.gis.geos import Point
 from django.http import HttpResponse
-from project.models import Stop
+from project.models import Stop, StopTime
 from vectorformats.Formats import Django, GeoJSON
 
 
 def stops(request):
-    django_format = Django.Django(geodjango='geom', properties=['id', 'name'])
-    geojson_format = GeoJSON.GeoJSON()
-
-    lat = float(request.GET.get('lat', 0))
     lon = float(request.GET.get('lon', 0))
+    lat = float(request.GET.get('lat', 0))
     radius = float(request.GET.get('radius', 800))
 
-    center = Point(lon, lat)
-    result = Stop.objects.filter(geom__distance_lte=(center, radius))
-    geojson = geojson_format.encode(django_format.decode(result))
+    stops = get_stops(lon, lat, radius)
+    geojson = queryset_to_geojson(stops)
 
     return HttpResponse(geojson, mimetype='application/json')
+
+
+def routes(request):
+    lon = float(request.GET.get('lon', 0))
+    lat = float(request.GET.get('lat', 0))
+    radius = float(request.GET.get('radius', 800))
+
+    stops = get_stops(lon, lat, radius)
+    route_shortnames = StopTime.objects.filter(
+        stop__in=stops).values('trip__route__short_name').distinct()
+
+    # return HttpResponse(geojson, mimetype='application/json')
+    return HttpResponse(
+        [obj['trip__route__short_name'] for obj in route_shortnames])
+
+
+def queryset_to_geojson(qs):
+    django_format = Django.Django(geodjango='geom', properties=['id', 'name'])
+    geojson_format = GeoJSON.GeoJSON()
+    return geojson_format.encode(django_format.decode(qs))
+
+
+def get_stops(lon, lat, radius):
+    center = Point(lon, lat)
+    return Stop.objects.filter(geom__distance_lte=(center, radius))
