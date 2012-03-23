1ace2ceafdf2704e879cedc2713899821360aea1
Aaron Ogle <atogle@gmail.com>
2012-03-23 13:01:50 -0400

    add stop view to return geojson

--- src/project/urls.py
+++ src/project/urls.py
@@ -16,5 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', TemplateView.as_view(template_name='index.html'))
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+
+    url(r'^stops/?$', 'project.views.stops', name='stops'),
 )
