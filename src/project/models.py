849e583a564db761fa02017fba35de6db6b7b469
Aaron Ogle <atogle@gmail.com>
2012-03-23 16:10:38 -0400

    add models for the rest of the gtfs tables

--- src/project/models.py
+++ src/project/models.py
@@ -2,13 +2,9 @@ from django.contrib.gis.db import models
 
 
 class Stop(models.Model):
-    # Regular Django fields corresponding to the attributes in the
-    # world borders shapefile.
-    id = models.PositiveIntegerField(primary_key=True, db_column="stop_id")
+    id = models.TextField(primary_key=True, db_column="stop_id")
     name = models.TextField(db_column="stop_name")
 
-    # GeoDjango-specific: a geometry field (MultiPolygonField), and
-    # overriding the default manager with a GeoManager instance.
     geom = models.PointField(db_column="the_geom")
     objects = models.GeoManager()
 
@@ -19,3 +15,65 @@ class Stop(models.Model):
     # Returns the string representation of the model.
     def __unicode__(self):
         return self.name
+
+
+class StopTime(models.Model):
+    id = models.PositiveIntegerField(
+        primary_key=True, db_column='stop_time_id')
+    stop = models.ForeignKey("Stop")
+    trip = models.ForeignKey("Trip")
+    departure_time = models.TextField()
+    departure_seconds = models.PositiveIntegerField(
+      db_column="departure_time_seconds")
+
+    class Meta:
+        db_table = "gtfs_stop_times"
+        managed = False
+
+    # Returns the string representation of the model.
+    def __unicode__(self):
+        return "%s, %s" % (self.stop.name, self.departure_time)
+
+
+class Trip(models.Model):
+    id = models.PositiveIntegerField(primary_key=True, db_column='trip_id')
+    headsign = models.TextField(db_column='trip_headsign')
+    direction_id = models.PositiveIntegerField()
+    shape = models.ForeignKey("Shape")
+    route = models.ForeignKey("Route")
+
+    class Meta:
+        db_table = "gtfs_trips"
+        managed = False
+
+    # Returns the string representation of the model.
+    def __unicode__(self):
+        return self.headsign
+
+
+class Route(models.Model):
+    id = models.PositiveIntegerField(primary_key=True, db_column='route_id')
+    short_name = models.TextField(db_column='route_short_name')
+    long_name = models.TextField(db_column='route_long_name')
+
+    class Meta:
+        db_table = "gtfs_routes"
+        managed = False
+
+    # Returns the string representation of the model.
+    def __unicode__(self):
+        return self.short_name
+
+
+class Shape(models.Model):
+    id = models.PositiveIntegerField(primary_key=True, db_column="shape_id")
+    geom = models.PointField(db_column="the_geom")
+    objects = models.GeoManager()
+
+    class Meta:
+        db_table = "gtfs_shape_geoms"
+        managed = False
+
+    # Returns the string representation of the model.
+    def __unicode__(self):
+        return self.id
